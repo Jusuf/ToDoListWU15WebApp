9eb1d4313c882078110895631d3858ce1a14ea94
jusuf_b <jusuf_b@hotmail.com>
2015-11-25 15:38:06 +0100

    ToDo done

---
+++ ToDoListWU15WebApp/DataAccess/DefaultDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ToDoListWU15WebApp.Models;
+
+namespace ToDoListWU15WebApp.DataAccess
+{
+    public class DefaultDataContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
